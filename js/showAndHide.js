16201f3998d914f2c6e73b1d22f986c01b049cf2
jobkaeHenry <1zzangjun@gmail.com>
2022-05-20 17:51:33 +0900

    added

--- js/showAndHide.js
+++ js/showAndHide.js
@@ -1,5 +1,9 @@
+
 var main = document.getElementById("main");
 var test = document.getElementById("test");
+// var ansBtn = document.getElementsByClassName("ans_button")
+// var lfAngle = document.getElementById("left_angle")
+// var riAngle = document.getElementById("right_angle")
 
 const startTest=()=>{
   main.style.display="none";
@@ -10,3 +14,13 @@ const backToMain=()=>{
   main.style.display="flex";
   test.style.display="none";
 }
+
+// const mouseHover=()=>{
+//     lfAngle.style.backgroundImage="url(../img/left_angle_filled.svg)"
+//     riAngle.style.backgroundImage="url(../img/right_angle_filled.svg)"
+// }
+
+// const mouseOut=()=>{
+//   lfAngle.style.backgroundImage="url(../img/left_angle.svg)"
+//   riAngle.style.backgroundImage="url(../img/right_angle.svg)"
+// }
