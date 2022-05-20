33c532166ffffe497c42848dc6bffa3de1e945a9
jobkaehenry <1zzangjun@gmail.com>
2022-05-21 04:01:58 +0900

    all page added

--- js/showAndHide.js
+++ js/showAndHide.js
@@ -1,6 +1,8 @@
 
 var main = document.getElementById("main");
 var test = document.getElementById("test");
+var result = document.getElementById("result")
+var resultShare = document.getElementById("result_share")
 // var ansBtn = document.getElementsByClassName("ans_button")
 // var lfAngle = document.getElementById("left_angle")
 // var riAngle = document.getElementById("right_angle")
@@ -14,6 +16,13 @@ const backToMain=()=>{
   main.style.display="flex";
   test.style.display="none";
 }
+
+const viewResult=()=>{
+  result.style.display="flex"
+  test.style.display="none"
+  resultShare.style.display="block"
+}
+
 
 // const mouseHover=()=>{
 //     lfAngle.style.backgroundImage="url(../img/left_angle_filled.svg)"
