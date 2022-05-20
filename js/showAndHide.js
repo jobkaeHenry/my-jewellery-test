cefdc2cdbb321375efd7216c1c7e3deefbc37435
jobkaeHenry <1zzangjun@gmail.com>
2022-05-20 13:55:53 +0900

    btn added

--- js/showAndHide.js
+++ js/showAndHide.js
@@ -1,1 +1,7 @@
-var get = document.getElementById()
+var main = document.getElementById("main");
+var test = document.getElementById("test")
+
+const startTest=()=>{
+  main.style.display="none";
+  test.style.display="flex";
+}
