63480f38a4e69b56e10f597e1734653c4418eaa4
jobkaeHenry <1zzangjun@gmail.com>
2022-05-20 13:59:56 +0900

    function added

--- js/showAndHide.js
+++ js/showAndHide.js
@@ -1,7 +1,12 @@
 var main = document.getElementById("main");
-var test = document.getElementById("test")
+var test = document.getElementById("test");
 
 const startTest=()=>{
   main.style.display="none";
   test.style.display="flex";
+}
+
+const backToMain=()=>{
+  main.style.display="flex";
+  test.style.display="none";
 }
