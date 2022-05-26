f9343bee1750f3b1334016affc8db30b8a692e82
jobkaeHenry <1zzangjun@gmail.com>
2022-05-26 16:19:10 +0900

    error fixed

--- js/getValue.js
+++ js/getValue.js
@@ -1,44 +1,74 @@
-$("#ans_01").click(function(){
-  var type = $("#whatType").val();
-  var preValue = $("#"+type).val();
-  $("#"+type).val(parseInt(preValue)+1);
-  next()
-});
-
-$("#ans_02").click(function(){
-  next()
-});
+//-------------------문제넘기기-----------------
 
 var questNum = 0;
-var next=()=>{
-  $(".howMuch").attr("style","width:calc(100/12*"+(parseInt(questNum))+"%)")
-  $("#question").html(questionSheet[questNum]["question"]);
+var next = () => {
+  $("#sheet").html(questionSheet[questNum]["sheet"]);
   $("#whatType").val(questionSheet[questNum]["type"]);
   $("#a").html(questionSheet[questNum]["a"]);
   $("#b").html(questionSheet[questNum]["b"]);
-  questNum++;
+  $(".howMuch").attr("style", "width:calc(100/12*" + (parseInt(questNum)) + "%)");
+  $("#howMuchNum").html(parseInt(questNum)+1+"/12")
+  questNum++
 };
 
+$("#ans_01").click(function () {
+  let type = $("#whatType").val();
+  let preValue = $("#" + type).val();
+  $("#" + type).val(parseInt(preValue) + 1);
+  next()
+});
 
+$("#ans_02").click(function () {
+  next()
+});
+
+//--------------------제이슨-------------------------
 var questionSheet = {
   0: {
-    "question": "첫 만남에 느낌이 좋았던 그 사람! 이후 연락이 오지않는다면?",
+    "sheet": "첫 만남에 느낌이 좋았던 그 사람! 이후 연락이 오지않는다면?",
     "type": "EI",
     "a": "먼저 연락해서 시간있냐고 물어본다",
-    "b": "자존심이 허락안해... 그냥 지나간다"
+    "b": "자존심이 허락안해... 그냥 지나간다",
   },
-  
+
   1: {
-    "question": " parapara",
+    "sheet": "parapara",
     "type": "EI",
-    "a": "에라모르겟다",
-    "b": "진짜모르겠네"
+    "a": "a1",
+    "b": "a2",
   },
 
   2: {
-    "question": "첫 만남에 ! 이후 연락이 오지않느낌이 좋았던 그 사람는다면?",
+    "sheet": "첫 만남에 ! 이후 연락이 오지않느낌이 좋았던 그 사람는다면?",
     "type": "EI",
-    "a": "먼저 락해서 시간있연냐고 물어본다",
-    "b": "자락안해... 그냥존심이 허 지나간다"
+    "a": "a2",
+    "b": "b2",
   },
 }
+
+
+// -------------------화면넘기기 함수-----------------------
+
+
+var main = document.getElementById("main");
+var test = document.getElementById("test");
+var result = document.getElementById("result");
+var resultShare = document.getElementById("result_share");
+
+
+var startTest = () => {
+  main.style.display = "none";
+  test.style.display = "flex";
+  next();
+}
+
+const backToMain = () => {
+  main.style.display = "flex";
+  test.style.display = "none";
+}
+
+// const viewResult=()=>{
+//   result.style.display="flex"
+//   test.style.display="none"
+//   resultShare.style.display="block"
+// }
