23db3c95ca567fc135e437084ce9bcb4798a91fd
jobkaehenry <1zzangjun@gmail.com>
2022-06-07 02:29:41 +0900

    all done finally

--- js/kakao.js
+++ js/kakao.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
   Kakao.init('d63a2454b6b5f2aa928a43fdee5ff8e0');
 
   function sendLink() { // 카카오톡 공유하기
@@ -17,4 +16,11 @@
       text: '[나만의 보석테스트] 인싸들의 필수코스! 나와 꼭 맞는 아름다운 보석을 간단한 심리테스트로 찾아보아요',
     })
   }
-</script>
+
+  function copyUrl (){
+    navigator.clipboard.writeText(location.href).then(() => {
+      console.log('success');
+  });
+
+    alert("링크가 복사되었습니다 붙혀넣기를 통해 공유해보세요")
+}
