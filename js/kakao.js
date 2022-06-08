d1086b417dd6636612af94398a5ad9f155f76bf1
jobkaehenry <1zzangjun@gmail.com>
2022-06-08 18:14:03 +0900

    try

--- js/kakao.js
+++ js/kakao.js
@@ -1,19 +1,23 @@
-
-
-
 function sendLink() { // 카카오톡 공유하기
   Kakao.init('d63a2454b6b5f2aa928a43fdee5ff8e0');
   Kakao.Link.sendDefault({
-    objectType: 'text',
-    text: '[나만의 보석테스트] 인싸들의 필수코스! 나와 꼭 맞는 아름다운 보석을 간단한 심리테스트로 찾아보아요',
-    link: {
-      mobileWebUrl: 'https://jobkaehenry.github.io/what-am-i-toy-project-/',
-      webUrl: 'https://jobkaehenry.github.io/what-am-i-toy-project-/',
-    },
+    objectType: 'feed',
+    content: {
+      title: '[나만의 보석테스트]',
+      description: '인싸들의 필수코스! 나와 꼭 맞는 아름다운 보석을 간단한 심리테스트로 찾아보아요',
+      imageUrl:
+        'https://ifh.cc/v-GB2Q55.jpg',
+      link: {
+        mobileWebUrl: 'https://jobkaehenry.github.io/what-am-i-toy-project-/',
+
+      },
+    }
+
+    // objectType: 'text',
+    // text: '[나만의 보석테스트] 인싸들의 필수코스! 나와 꼭 맞는 아름다운 보석을 간단한 심리테스트로 찾아보아요',
+    // link: {
+    //   mobileWebUrl: 'https://jobkaehenry.github.io/what-am-i-toy-project-/',
+    //   webUrl: 'https://jobkaehenry.github.io/what-am-i-toy-project-/',
+    // },
   })
 }
-function shareStoryWeb() { // 카카오 스토리 공유하기
-  Kakao.Story.share({
-    url: 'https://jobkaehenry.github.io/what-am-i-toy-project-/',
-    text: '[나만의 보석테스트] 인싸들의 필수코스! 나와 꼭 맞는 아름다운 보석을 간단한 심리테스트로 찾아보아요',
-  })}
