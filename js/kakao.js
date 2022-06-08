fcad159a0deb3705efaa7581c734cd80a395e21d
jobkaehenry <1zzangjun@gmail.com>
2022-06-08 18:16:39 +0900

    img changed

--- js/kakao.js
+++ js/kakao.js
@@ -6,7 +6,7 @@ function sendLink() { // 카카오톡 공유하기
       title: '[나만의 보석테스트]',
       description: '인싸들의 필수코스! 나와 꼭 맞는 아름다운 보석을 간단한 심리테스트로 찾아보아요',
       imageUrl:
-        'https://ifh.cc/v-GB2Q55.jpg',
+        'https://ifh.cc/g/hBk9P2.jpg',
       link: {
         mobileWebUrl: 'https://jobkaehenry.github.io/what-am-i-toy-project-/',
 
